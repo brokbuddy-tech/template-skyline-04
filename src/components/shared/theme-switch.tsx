a2c5d53cdaa8b5de604c9d905f35be78446f2f7c
Aditya tiwari <adityatiwari96728@gmail.com>
2025-11-15 15:57:36 +0000

    for the theme switcher , add a black stroke for light theme , and a whit

--- src/components/shared/theme-switch.tsx
+++ src/components/shared/theme-switch.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'next-themes';
 import { Moon, Sun } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
@@ -13,9 +14,17 @@ export function ThemeSwitch() {
     setMounted(true);
   }, []);
 
+  const buttonClasses =
+    'rounded-full w-[60px] h-[60px] border dark:border-white border-black';
+
   if (!mounted) {
     return (
-      <Button variant="outline" size="icon" disabled className="rounded-full w-[60px] h-[60px]">
+      <Button
+        variant="outline"
+        size="icon"
+        disabled
+        className={cn(buttonClasses)}
+      >
         <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
@@ -27,7 +36,7 @@ export function ThemeSwitch() {
     <Button
       variant="outline"
       size="icon"
-      className="rounded-full w-[60px] h-[60px]"
+      className={cn(buttonClasses)}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
